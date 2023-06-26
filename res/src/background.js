45b0363a9ad75e51a8233dfbab04e0a0e6fc9a39
Samuel302 <samuel.mischl@gamingcraft.de>
2023-06-26 20:12:40 +0000

    Fix Pronouns &

--- res/src/background.js
+++ res/src/background.js
@@ -4,7 +4,7 @@ const onscroll = async () => {
     const height = document.body.offsetHeight
     const width = document.body.offsetWidth
     const scrollPx = getScrollPercent() * (height-width+64)
-    document.getElementById('body').style.backgroundPositionY = `${scrollPx}px`
+    document.querySelector('body').style.backgroundPositionY = `${scrollPx}px`
 }
 
 onscroll()
