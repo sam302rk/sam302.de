21efdfb567c28360058806e4bbb398e0a5f777c9
Sam <sam@gamingcraft.de>
2024-07-15 20:19:53 +0200

    Blog: Block.

--- res/src/blog.js
+++ res/src/blog.js
@@ -1,3 +1,5 @@
+import { codeToHtml } from 'https://esm.sh/shiki@1.0.0'
+
 let generateArticlePreview =
     (title, createdAt, descr, file) => { return `<p>* <a href="/blog/?article=${file}" class="title">${title}</a> <span class="comment">// ${createdAt}</span><br>↳ ${descr}</p>` }
 
@@ -6,6 +8,46 @@ if (params.article) {
 
     fetch(`/blog/articles/${params.article}.md`).then(async res => {
         document.getElementById('article').innerHTML += marked.parse(await res.text())
+
+        document.getElementById('article').querySelectorAll("pre").forEach(async pre => {
+            const code = pre.querySelector("code")
+
+            let lang = "txt"
+            code.classList.forEach(element => {
+                if (element.startsWith("language-")) lang = element.replace("language-", '')
+            })
+
+            const generated = await codeToHtml(code.innerHTML, {
+                lang: lang,
+                theme: 'catppuccin-mocha'
+            })
+
+            let storage = document.createElement('div')
+            storage.innerHTML = generated
+            code.innerHTML = ""
+
+            let count = 1
+            let table = document.createElement('table')
+
+            storage.querySelectorAll('span.line').forEach(line => {
+                let tr = document.createElement('tr')
+                
+                let td_line = document.createElement('td')
+                td_line.classList.add('line')
+                td_line.innerHTML = count
+                count += 1
+                
+                let td_code = document.createElement('td')
+                td_code.classList.add('code')
+                td_code.append(line)
+
+                tr.append(td_line)
+                tr.append(td_code)
+                table.append(tr)
+            })
+
+            code.append(table)
+        })
     })
 } else {
     document.getElementById('arwin').classList.add('hide')
